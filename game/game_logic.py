9e42f7c9c523a4df8743719ff2ba7180a1c5e837
Edmond Ipindamitan <eipindamitan@VONFVFWT2XAHV2H.local>
2018-11-29 18:57:58 +0000

    Game logic.

---
+++ game/game_logic.py
@@ -0,0 +1,10 @@
+
+class GameMaster(object):
+    def __init__(self, name, game_id=0):
+        self.name = name
+        self.load(game_id)
+
+    def _load(self, game_id):
+
+
+    def receive_answer(self):
