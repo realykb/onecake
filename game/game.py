44c4af2734d583aa7f1cc34e0cd4dd4edfa22c4a
Aurele Lenfant <aurele.lenfant@vonage.com>
2018-11-30 10:24:27 +0000

    More game stuff

--- game/game.py
+++ game/game.py
@@ -10,12 +10,8 @@ class Game(object):
     def _load(self, file_path):
         with open(file_path, 'rt') as file_content:
             self.game_json = json.load(file_content)
-            self.credits = self.game_json['credits']
-            self.game_json = self.game_json['questions']
 
     def get_next_step(self, id, content_type, content):
-        if id in self.sessions:
-            return self.sessions[id].get_next_step(content_type, content)
-        else:
+        if id not in self.sessions:
             self.sessions[id] = Session(self.game_json)
-            return self.sessions[id]
+        return self.sessions[id].get_next_step(content_type, content)
--- game/session.py
+++ game/session.py
@@ -1,44 +1,58 @@
 class Session:
     def __init__(self, game_json):
         self.game_json = game_json
-        self.current_step_id = self.game_json['steps'][0]['id']
+        self.current_step = self.get_step(1)
+
+    def get_step(self, id):
+        for step in self.game_json['steps']:
+            if step['id'] == id:
+                return step
+        return None
+
+    def get_current_step(self):
+        for step in self.game_json['steps']:
+            if step['id'] == id:
+                return step
+        return None
 
     def get_next_step(self, answer_type, answer_content):
-        steps = self.game_json['steps']
-        current_step = steps[self.current_step_id]
-        expected_type = current_step['expected']['type']
-        expected_content = current_step['expected']['content']
+        expected_type = self.current_step['expected']['type']
+        expected_content = self.current_step['expected']['content']
 
-        if expected_type != answer_type:
+        if answer_type not in expected_type:
             return self.get_next_step_faillure()
 
-        if expected_content == 'text':
+        if 'text' in expected_type:
             if self.text_match(expected_content, answer_content):
                 return self.get_next_step_success()
+            else:
+                return self.get_next_step_faillure()
 
     def text_match(self, expected, actual):
-        words = actual.split("")
+        words = actual.split()
         for word in words:
-            if expected.lower() == word.lower():
+            if word.lower() in expected:
+                print('true')
                 return True
+        print('false')
         return False
 
     def get_next_step_success(self):
-        steps = self.game_json['steps']
-        current_step = steps[self.current_step_id]
-        if 'content' in current_step['on_success']:
-            self.current_step_id = current_step['on_success']['content']
-            current_step = steps[self.current_step_id]
-            return current_step['type'], current_step['message']
-        else:
+        if 'on_success' not in self.current_step:
             return None
+        if 'content' in self.current_step['on_success']:
+            next_step_id = self.current_step['on_success']['content']
+            current_step = self.get_step(next_step_id)
+            return True, current_step['message']['type'], current_step['message']['content']
+        else:
+            return True, None, None
 
     def get_next_step_faillure(self):
-        steps = self.game_json['steps']
-        current_step = steps[self.current_step_id]
-        if 'content' in current_step['on_failure']:
-            self.current_step_id = current_step['on_failure']['content']
-            current_step = steps[self.current_step_id]
-            return current_step['type'], current_step['message']
-        else:
+        if 'on_failure' not in self.current_step:
             return None
+        if 'content' in self.current_step['on_failure']:
+            next_step_id = self.current_step['on_failure']['content']
+            current_step = self.get_step(next_step_id)
+            return False, current_step['message']['type'], current_step['message']['content']
+        else:
+            return False, None, None
